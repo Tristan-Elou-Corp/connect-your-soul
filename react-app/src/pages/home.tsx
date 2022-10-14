68ee2198acffc4ec486bd9b39bacd11564450731
tristangory <tristan.gory@epitech.eu>
2022-10-14 15:19:57 +0200

    [ADD] Added style for the home page and some text

--- react-app/src/pages/home.tsx
+++ react-app/src/pages/home.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import Header from "../components/header";
+import "../scss/home.scss";
 
 function Home() {
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+      <div className="citation-wrapper">
+        <h2 className="citation-1">
+          Un accompagnement personnel,
+        </h2>
+        <h2 className="citation-2">
+          intégratif et holistique
+        </h2>
+      </div>
     </div>
   );
 }
