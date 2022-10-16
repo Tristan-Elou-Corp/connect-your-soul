015ca35b652633eb2cd72d1ff30585d17531af98
Elouan Da Costa Peixoto <78272327+ElouanDaCosta@users.noreply.github.com>
2022-10-16 18:24:21 +0200

    [FIXING] the _home.scss file and the className of the home page

--- react-app/src/pages/home.tsx
+++ react-app/src/pages/home.tsx
@@ -22,46 +22,46 @@ function Home() {
   return (
     <div className="home-wrapper">
       <Header />
-      <div className="citation-wrapper">
-        <h2 className="citation-1">
+      <div className="home-wrapper__citation-wrapper">
+        <h2 className="home-wrapper__citation-wrapper__citation">
           Un accompagnement personnel,
         </h2>
-        <h2 className="citation-2">
+        <h2 className="home-wrapper__citation-wrapper__citation">
           intégratif et holistique
         </h2>
       </div>
-      <img src={oiseaucage} alt="oiseaucage" className="oiseaucage"/>
-      <p className="citation-3-wrapper">
-        <span className="citation-3">
+      <img src={oiseaucage} alt="oiseaucage" className="home-wrapper__oiseaucage"/>
+      <p className="home-wrapper__citation">
+        <span className="home-wrapper__citation__text">
           J’ai tenté de faire comme ce colibri : sortir de cette cage pour comprendre ce qu’est la liberté pour être en paix, vivre en paix. Mais je me suis heurtée à de nombreux obstacles qui me ramenaient à ma réalité : nous ne pouvons pas vivre déconnectés de notre environnement… Alors comment exprimer sa singularité et sa liberté dans ce monde régi par des codes et enfermé dans un cadre ?
         </span>
       </p>
-      <p>
-        <span className="title-1">
+      <p className="home-wrapper__citation_title">
+        <span className="home-wrapper__citation_title__title">
           Ma vision du changement de paradigmes pour demain
         </span>
       </p>
-      <p className="citation-4-1-wrapper">
-        <span className="citation-4-1">
+      <p className="home-wrapper__citation--normal">
+        <span className="home-wrapper__citation--normal__text">
           Par mes formations intégratives « sur mesure » et mon coaching holistique, je suis dans le partage de ce que la vie vous/nous apprend. Et je ressens plus que jamais que nous devons mettre des mots aux maux de ce monde et panser notre intérieur pour vivre pleinement notre destinée professionnelle et personnelle. #calibrateyourheart
         </span>
       </p>
-      <p className="citation-4-2-wrapper">
-        <span className="citation-4-2">
+      <p className="home-wrapper__citation--normal">
+        <span className="home-wrapper__citation--normal__text">
         La vie, celle de « madame ou monsieur tout le monde » doit être calibrée en fonction de ce que l’on est au fond de soi. Vivre ne doit pas être de subir ou réagir mais plutôt de s’accepter soi-même d’abord, accepter les autres puis accepter le monde. Nous vivons dans un monde de reproches… Nous vivons du reproche, par le reproche et en fonction des reproches de la perfection et j’irai plus loin de la « perfectude »… C’est en fait une société mondiale basée sur le meilleur qui régit nos vies., cette même société ne se rend pas compte qu’elle nous nivelle vers le bas tout en pensant nous améliorer et nous élever vers le rang des meilleurs. 
         </span>
       </p>
-      <p className="citation-4-3-wrapper">
-        <span className="citation-4-3">
+      <p className="home-wrapper__citation--normal">
+        <span className="home-wrapper__citation--normal__text">
         La comparaison et la compétition (deux mécanismes clés de l’Ego) semblent faire du bien à cet humain en nous qui a besoin d’être challengé, boosté, et valorisé pour ses efforts; Et si nous prenions l’équation à l’envers: cet même humain fait des efforts car il ne serait ni comparé ni compétiteur pour être valorisé, boosté et challengé.
         </span>
       </p>
-      <p className="citation-4-4-wrapper">
-        <span className="citation-4-4">
+      <p className="home-wrapper__citation--normal">
+        <span className="home-wrapper__citation--normal__text">
         D’où ce reproche inconscient de l’humanité…
         </span>
       </p>
-      <ul className="list">
+      <ul className="home-wrapper__list">
         <li>
           <span>
             Qui es-tu aux yeux du monde pour être reconnu?
@@ -88,39 +88,39 @@ function Home() {
           </span>
         </li>
       </ul>
-      <p className="citation-5-wrapper">
-        <span className="citation-5">
+      <p className="home-wrapper__citation--normal">
+        <span className="home-wrapper__citation--normal__text">
           Tant de reproches tournés positivement qui hantent nos vies depuis notre plus jeune âge avec nos parents, à l’école, au travail, dans un couple, dans les médias… 
         </span>
       </p>
-      <p className="citation-6-wrapper">
-        <span className="citation-6">
+      <p className="home-wrapper__citation--normal">
+        <span className="home-wrapper__citation--normal__text">
         Tout notre questionnement repose sur le sens même du mot « existence » :
         </span>
       </p>
-      <div className="citation-7-div-wrapper">
-        <h2 className="citation-7-title-wrapper">
-          <span className="citation-7-title">
+      <div className="home-wrapper__citation--background">
+        <h2 className="home-wrapper__citation--background__title_block">
+          <span className="home-wrapper__citation--background__title_block__title">
             EST-CE QUE J’EXISTE POUR OUBLIER QUI JE SUIS, OU BIEN, TOUT SIMPLEMENT J’EXISTE PARCE QUE JE SUIS ? 
           </span>
         </h2>
-        <p className="citation-7-1-wrapper">
-          <span className="citation-7-1">
+        <p className="home-wrapper__citation--background__citation">
+          <span className="home-wrapper__citation--background__citation__text">
             La liberté n’est vraie que lorsque vous avez la paix au fond de votre cœur comme un moteur puissant qui accepte les obstacles et l’adversité de la vie.
           </span>
         </p>
-        <p className="citation-7-2-wrapper">
-          <span className="citation-7-2">
+        <p className="home-wrapper__citation--background__citation">
+          <span className="home-wrapper__citation--background__citation__text">
             Si vous pensez détenir la vérité, les connaissances, la sagesse ou la puissance ; si vous manquez de reconnaissance, notoriété ou d’influence… Alors mon coaching est pour vous, je vous accompagnerai dans votre quotidien, dans le partage et la
           </span>
         </p>
-        <p className="citation-7-3-wrapper">
-          <span className="citation-7-3">
+        <p className="home-wrapper__citation--background__citation">
+          <span className="home-wrapper__citation--background__citation__text">
             bienveillance pour transcender votre For intérieur afin qu’il rayonne vers le For extérieur.
           </span>
         </p>
       </div>
-      <div className="three-square-wrapper">
+      <div className="home-wrapper__square">
         <div className="first-square-wrapper">
           <div className="first-square">
             <img src={puzzlepiece} alt="puzzlepiece" />
